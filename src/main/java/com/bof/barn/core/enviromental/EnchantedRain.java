f56c0d7ba5a5a98c6262fed123ac90406e6f659c
Wega <tomous.wega@gmail.com>
2023-10-01 19:33:10 +0200

    changes to EnchantedRain

--- src/main/java/com/bof/barn/core/enviromental/EnchantedRain.java
+++ src/main/java/com/bof/barn/core/enviromental/EnchantedRain.java
@@ -1,33 +1,30 @@
 package com.bof.barn.core.enviromental;
 
-import lombok.Data;
-
 import java.util.Random;
 
 import static com.bof.barn.core.Core.WORLD;
 
-@Data
 public class EnchantedRain {
     private final Random random = new Random();
-    private boolean isRunning = false;
+    public static boolean isRunning = false;
 
     public boolean shouldStartEnchantedRain() {
        return this.random.nextDouble() <= 0.075;
     }
 
     public boolean start() {
-        if (this.isRunning) return false;
+        if (isRunning) return false;
 
         WORLD.setStorm(true);
-        this.isRunning = true;
+        isRunning = true;
 
         return true;
     }
 
     public boolean stop() {
-        if (this.isRunning) {
+        if (isRunning) {
             WORLD.setStorm(false);
-            this.isRunning = false;
+            isRunning = false;
             return true;
         }
         return false;
